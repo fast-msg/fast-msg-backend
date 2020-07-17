f0a5d1ded7d70ec2c966bc01d799dbf32e581016
Emely Garcia <garciam.emm@gmail.com>
2020-07-17 15:22:01 -0600

    mensajes a usuarios especificos

--- index.js
+++ index.js
@@ -7,21 +7,40 @@ const port = 8080;
 const options = { /* ... */ };
 const io = require('socket.io')(http, options);
 
+var users = [];
 
 //configuración de sockets
-io.on('connection',(socket) => {
-    console.log('a user connected');
+io.on('connection', (socket) => {
     //broadcast para todos menos el que envió el mensaje
-    socket.broadcast.emit('new-user','Nuevo usuario conectado.'); 
+    socket.broadcast.emit('new-user', 'Nuevo usuario conectado.');
     socket.on('chat message', (msg) => {
-        //envia a todos los usuarios
-        io.emit('chat message', msg);
-      });
-  });
+        console.log(msg)
+        var user = users.find((value) => value.nombre === msg.to);
+        console.log(user)
+        if (user) {
+            io.to(socket.id).emit('chat message',msg);
+            io.to(user.socketId).emit('chat message',msg);
+        }
+    });
+
+    socket.on('register', (username) => {
+        addUser(username, socket.id);
+        io.to(socket.id).emit('chat message',{from:'Fast-Messages',to:username,mensaje:'Bienvenido '+username});
+    });
+});
+
+function addUser(username, socketId) {
+    var user = users.find((value) => value.nombre === username);
+    if (user) {
+        user.socketId = socketId
+    } else {
+        users.push({ nombre: username, socketId })
+    }
+}
 
 
 //configuración de http
-http.listen(port,()=>{
+http.listen(port, () => {
     console.log("Server listen on port 8080");
 });
 
--- public/main.js
+++ public/main.js
@@ -1,19 +1,26 @@
 $(document).ready(function () {
     console.log("iniciando...")
-    var socket = io.connect();
+    var socket = io.connect({ query: "foo=bar" });
+
+    $('#registrar').click(function (e) { 
+      e.preventDefault();
+      console.log('registrando...');
+        socket.emit('register',$("#nombre").val());
+    });
 
     $('#send-message').submit(function (e) { 
         e.preventDefault();
-        var nombre = $("#nombre").val()
+        var from = $("#nombre").val()
+        var to = $("#to").val()
         var mensaje = $('#mensaje')
-        socket.emit('chat message', {nombre,mensaje:mensaje.val()});
+        socket.emit('chat message',{from,to,mensaje:mensaje.val()});
         mensaje.val('');
       return false;    
     });
 
     socket.on('chat message', function(msg){
         $('#container-messages')
-        .append('<p><span>'+msg.nombre+':</span>'+msg.mensaje+'<p>');
+        .append('<p><span>'+msg.from+':</span>'+msg.mensaje+'<p>');
       });
 
     socket.on('new-user',(object)=>{
