47d74e46789c3d1662a66b53f57ec595dc9bc96e
Emely Garcia <garciam.emm@gmail.com>
2020-07-20 08:56:51 -0600

    eliminando virtuals

--- src/app/models/user.model.js
+++ src/app/models/user.model.js
@@ -3,30 +3,21 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema;
 
 var Schema = schema({
-    name: String,
-    email: String,
-    password: String,
-    status: Number,
-    image: String,
-    contacts: [String],
-    chats: [String],
-    settings: {
-        theme: String
-    }
-}, {
-    toObject: {
-        virtuals: true
-    },
-    toJSON: {
-        virtuals: true
+    name:String,
+    email:String,
+    password:String,
+    status:Number,
+    image:String,
+    contacts:[String],
+    chats:[String],
+    settings:{
+        theme:String
     }
 });
-
-Schema.virtual('id').get(function () { return this._id; });
 
 //---------STATUS----------------
 //1 - active
 //2 - desactivate
 //3 - congelate
 
-module.exports = mongoose.model('User', Schema);
+module.exports = mongoose.model('User',Schema);
